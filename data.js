84ee496d06a4029352f55df83c2e5fdcd4271768
xylit0l <huang335100@gmail.com>
2015-08-04 18:21:14 +0800

    2015/08/04
    自动化运维Demo

---
+++ data.js
@@ -0,0 +1,185 @@
+{
+    "sEcho": 3,
+    "iTotalRecords": 57,
+    "iTotalDisplayRecords": 57,
+    "aaData": [
+        [
+            "<input type="checkbox" />",
+            "10011",
+            "上海电信",
+            "24.32.84.1",
+            "<label class="label label-success">正常</label>",
+            "Centos",
+            "vangen"
+        ],
+        [
+            "<input type="checkbox" />",
+            "10011",
+            "上海电信",
+            "24.32.84.1",
+            "<label class="label label-success">正常</label>",
+            "Centos",
+            "vangen"
+        ],[
+            "<input type="checkbox" />",
+            "10011",
+            "上海电信",
+            "24.32.84.1",
+            "<label class="label label-success">正常</label>",
+            "Centos",
+            "vangen"
+        ],[
+            "<input type="checkbox" />",
+            "10012",
+            "湖南电信",
+            "47.32.84.1",
+            "<label class="label label-success">正常</label>",
+            "ubuntu",
+            "vangen"
+        ],[
+            "<input type="checkbox" />",
+            "10012",
+            "湖南电信",
+            "47.32.84.1",
+            "<label class="label label-success">正常</label>",
+            "ubuntu",
+            "vangen"
+        ],[
+            "<input type="checkbox" />",
+            "10012",
+            "湖南电信",
+            "47.32.84.1",
+            "<label class="label label-success">正常</label>",
+            "ubuntu",
+            "vangen"
+        ],[
+            "<input type="checkbox" />",
+            "10013",
+            "湖南联通",
+            "47.39.83.1",
+            "<label class="label label-warning">故障</label>",
+            "ubuntu",
+            "vangen"
+        ],[
+            "<input type="checkbox" />",
+            "10016",
+            "江西移动",
+            "37.39.83.1",
+            "<label class="label label-success">正常</label>",
+            "win7",
+            "vangen"
+        ],[
+            "<input type="checkbox" />",
+            "10016",
+            "江西移动",
+            "37.39.83.1",
+            "<label class="label label-success">正常</label>",
+            "win7",
+            "vangen"
+        ],[
+            "<input type="checkbox" />",
+            "10016",
+            "江西移动",
+            "37.39.83.1",
+            "<label class="label label-success">正常</label>",
+            "win7",
+            "vangen"
+        ],[
+            "<input type="checkbox" />",
+            "10016",
+            "江西移动",
+            "37.39.83.1",
+            "<label class="label label-success">正常</label>",
+            "win7",
+            "vangen"
+        ],[
+            "<input type="checkbox" />",
+            "10017",
+            "山西移动",
+            "37.32.83.1",
+            "<label class="label label-success">正常</label>",
+            "win7",
+            "vangen"
+        ],[
+            "<input type="checkbox" />",
+            "10018",
+            "河北电信",
+            "37.32.83.1",
+            "<label class="label label-success">正常</label>",
+            "win7",
+            "vangen"
+        ],[
+            "<input type="checkbox" />",
+            "10018",
+            "河北电信",
+            "37.32.83.1",
+            "<label class="label label-success">正常</label>",
+            "win7",
+            "vangen"
+        ],[
+            "<input type="checkbox" />",
+            "10018",
+            "河北电信",
+            "37.32.83.1",
+            "<label class="label label-success">正常</label>",
+            "win7",
+            "vangen"
+        ],[
+            "<input type="checkbox" />",
+            "10018",
+            "河北电信",
+            "37.32.83.1",
+            "<label class="label label-success">正常</label>",
+            "win7",
+            "vangen"
+        ],[
+            "<input type="checkbox" />",
+            "10018",
+            "河北电信",
+            "37.32.83.1",
+            "<label class="label label-success">正常</label>",
+            "win7",
+            "vangen"
+        ],[
+            "<input type="checkbox" />",
+            "10019",
+            "北京联通",
+            "17.32.83.1",
+            "<label class="label label-success">正常</label>",
+            "win7",
+            "vangen"
+        ],[
+            "<input type="checkbox" />",
+            "10019",
+            "北京联通",
+            "17.32.83.1",
+            "<label class="label label-success">正常</label>",
+            "win7",
+            "vangen"
+        ],[
+            "<input type="checkbox" />",
+            "10019",
+            "北京联通",
+            "17.32.83.1",
+            "<label class="label label-success">正常</label>",
+            "win7",
+            "vangen"
+        ],[
+            "<input type="checkbox" />",
+            "10019",
+            "北京联通",
+            "17.32.83.1",
+            "<label class="label label-success">正常</label>",
+            "win7",
+            "vangen"
+        ],[
+            "<input type="checkbox" />",
+            "10020",
+            "重庆联通",
+            "17.32.23.1",
+            "<label class="label label-success">正常</label>",
+            "linux mint",
+            "vangen"
+        ]
+    ]
+}
